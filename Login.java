85605f964e079c617d553fc82c725fdc72f9ab3b
argkan13 <83040380+argkan13@users.noreply.github.com>
2021-06-03 20:16:23 +0300

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,100 @@
+package erg1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Button;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import java.awt.TextField;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+
+public class Login extends JFrame {
+	
+
+	private JPanel contentPane;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	public Login() {
+		setBackground(Color.DARK_GRAY);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 794, 501);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		Button button = new Button("Log In");
+		button.setBounds(484, 276, 117, 44);
+		contentPane.add(button);
+		
+		Button button_1 = new Button("Register");
+		button_1.setBounds(484, 338, 117, 44);
+		contentPane.add(button_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/erg1/images/reservation+.png")));
+		lblNewLabel.setBounds(179, 50, 422, 178);
+		contentPane.add(lblNewLabel);
+		
+		TextField textField = new TextField();
+		textField.setBounds(179, 276, 138, 30);
+		contentPane.add(textField);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(179, 335, 138, 30);
+		contentPane.add(passwordField);
+		
+		JLabel lblNewLabel_1 = new JLabel("Username");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(179, 257, 94, 20);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(179, 316, 94, 20);
+		contentPane.add(lblNewLabel_1_1);
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+	}
+}
+
