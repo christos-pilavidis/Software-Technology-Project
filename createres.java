46bc78de3e59de5cf92451e6f61156e6e25f8915
christos-pilavidis <73226277+christos-pilavidis@users.noreply.github.com>
2021-06-11 04:06:40 +0300

    Update createres.java

--- createres.java
+++ createres.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
+import java.awt.Image;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -14,8 +16,9 @@ import javax.swing.border.LineBorder;
 import java.awt.Button;
 import com.toedter.calendar.JDateChooser;
 import com.toedter.calendar.JCalendar;
+import javax.swing.UIManager;
 
-public class create_res extends JFrame {
+public class createres extends JFrame {
 
 	private JPanel contentPane;
 
@@ -26,7 +29,7 @@ public class create_res extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					create_res frame = new create_res();
+					createres frame = new createres();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -38,125 +41,170 @@ public class create_res extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public create_res() {
+	public createres() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1250, 730);
+		setBounds(100, 100, 1000, 730);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(51, 51, 51));
-		contentPane.setBorder(new LineBorder(new Color(153, 0, 0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		Label welcome_label = new Label("Welcome to Reservation+");
 		welcome_label.setAlignment(Label.CENTER);
 		welcome_label.setFont(new Font("Calibri", Font.BOLD, 15));
-		welcome_label.setForeground(new Color(204, 0, 0));
+		welcome_label.setForeground(new Color(255, 255, 255));
 		welcome_label.setBackground(new Color(51, 51, 51));
-		welcome_label.setBounds(0, 0, 351, 37);
+		welcome_label.setBounds(-69, 0, 337, 26);
 		contentPane.add(welcome_label);
 		
 		TextField textTable = new TextField();
+		textTable.setBackground(UIManager.getColor("Button.light"));
 		textTable.setForeground(new Color(0, 0, 0));
 		textTable.setFont(new Font("Calibri", Font.PLAIN, 15));
-		textTable.setBounds(726, 111, 233, 37);
+		textTable.setBounds(628, 126, 233, 30);
 		contentPane.add(textTable);
 		
-		Label create_res_label = new Label("Create Reservation");
-		create_res_label.setFont(new Font("Calibri", Font.BOLD, 15));
-		create_res_label.setForeground(new Color(204, 0, 0));
-		create_res_label.setBackground(new Color(51, 51, 51));
-		create_res_label.setAlignment(Label.CENTER);
-		create_res_label.setBounds(790, 0, 446, 37);
-		contentPane.add(create_res_label);
+		Label createres_label = new Label("Create Reservation");
+		createres_label.setFont(new Font("Calibri", Font.BOLD, 15));
+		createres_label.setForeground(new Color(255, 255, 255));
+		createres_label.setBackground(new Color(51, 51, 51));
+		createres_label.setAlignment(Label.CENTER);
+		createres_label.setBounds(440, 0, 446, 37);
+		contentPane.add(createres_label);
 		
 		TextField textTable_1 = new TextField();
 		textTable_1.setForeground(Color.BLACK);
 		textTable_1.setFont(new Font("Calibri", Font.PLAIN, 15));
-		textTable_1.setBounds(726, 189, 233, 37);
+		textTable_1.setBounds(628, 196, 233, 30);
 		contentPane.add(textTable_1);
 		
 		TextField textTable_2 = new TextField();
 		textTable_2.setForeground(Color.BLACK);
 		textTable_2.setFont(new Font("Calibri", Font.PLAIN, 15));
-		textTable_2.setBounds(726, 267, 233, 37);
+		textTable_2.setBounds(628, 267, 233, 30);
 		contentPane.add(textTable_2);
 		
 		TextField textTable_3 = new TextField();
 		textTable_3.setForeground(Color.BLACK);
 		textTable_3.setFont(new Font("Calibri", Font.PLAIN, 15));
-		textTable_3.setBounds(726, 347, 233, 37);
+		textTable_3.setBounds(628, 343, 233, 30);
 		contentPane.add(textTable_3);
 		
 		TextField textTable_4 = new TextField();
+		textTable_4.setBackground(UIManager.getColor("Button.light"));
 		textTable_4.setForeground(Color.BLACK);
 		textTable_4.setFont(new Font("Calibri", Font.PLAIN, 15));
-		textTable_4.setBounds(726, 424, 233, 37);
+		textTable_4.setBounds(628, 420, 233, 30);
 		contentPane.add(textTable_4);
 		
 		TextField textTable_5 = new TextField();
 		textTable_5.setForeground(Color.BLACK);
 		textTable_5.setFont(new Font("Calibri", Font.PLAIN, 15));
-		textTable_5.setBounds(726, 508, 233, 87);
+		textTable_5.setBounds(628, 497, 233, 48);
 		contentPane.add(textTable_5);
 		
-		Button create_res_button = new Button("Create Reservation");
-		create_res_button.setBackground(new Color(51, 51, 51));
-		create_res_button.setForeground(new Color(255, 255, 255));
-		create_res_button.setFont(new Font("Calibri", Font.BOLD, 13));
-		create_res_button.setBounds(1001, 621, 200, 48);
-		contentPane.add(create_res_button);
+		Button createres_button = new Button("Create Reservation");
+		createres_button.setBackground(Color.LIGHT_GRAY);
+		createres_button.setForeground(Color.BLACK);
+		createres_button.setFont(new Font("Calibri", Font.BOLD, 13));
+		createres_button.setBounds(628, 621, 233, 48);
+		contentPane.add(createres_button);
 		
-		Label label_create_res = new Label("Table:");
-		label_create_res.setFont(new Font("Calibri", Font.PLAIN, 12));
-		label_create_res.setForeground(new Color(255, 255, 255));
-		label_create_res.setBounds(726, 84, 59, 21);
-		contentPane.add(label_create_res);
+		Label label_createres = new Label("Table:");
+		label_createres.setFont(new Font("Calibri", Font.PLAIN, 12));
+		label_createres.setForeground(new Color(255, 255, 255));
+		label_createres.setBounds(628, 99, 59, 21);
+		contentPane.add(label_createres);
 		
-		Label label_create_res_1 = new Label("Name:");
-		label_create_res_1.setForeground(Color.WHITE);
-		label_create_res_1.setFont(new Font("Calibri", Font.PLAIN, 12));
-		label_create_res_1.setBounds(726, 162, 87, 21);
-		contentPane.add(label_create_res_1);
+		Label label_createres_1 = new Label("Name:");
+		label_createres_1.setForeground(Color.WHITE);
+		label_createres_1.setFont(new Font("Calibri", Font.PLAIN, 12));
+		label_createres_1.setBounds(628, 169, 87, 21);
+		contentPane.add(label_createres_1);
 		
-		Label label_create_res_2 = new Label("Number of People:");
-		label_create_res_2.setForeground(Color.WHITE);
-		label_create_res_2.setFont(new Font("Calibri", Font.PLAIN, 12));
-		label_create_res_2.setBounds(726, 240, 132, 21);
-		contentPane.add(label_create_res_2);
+		Label label_createres_2 = new Label("Number of People:");
+		label_createres_2.setForeground(Color.WHITE);
+		label_createres_2.setFont(new Font("Calibri", Font.PLAIN, 12));
+		label_createres_2.setBounds(628, 240, 132, 21);
+		contentPane.add(label_createres_2);
 		
-		Label label_create_res_3 = new Label("Time of Arrival:");
-		label_create_res_3.setForeground(Color.WHITE);
-		label_create_res_3.setFont(new Font("Calibri", Font.PLAIN, 12));
-		label_create_res_3.setBounds(726, 321, 109, 21);
-		contentPane.add(label_create_res_3);
+		Label label_createres_3 = new Label("Time of Arrival:");
+		label_createres_3.setForeground(Color.WHITE);
+		label_createres_3.setFont(new Font("Calibri", Font.PLAIN, 12));
+		label_createres_3.setBounds(628, 316, 109, 21);
+		contentPane.add(label_createres_3);
 		
-		Label label_create_res_4 = new Label("Contact Number:");
-		label_create_res_4.setForeground(Color.WHITE);
-		label_create_res_4.setFont(new Font("Calibri", Font.PLAIN, 12));
-		label_create_res_4.setBounds(726, 397, 132, 21);
-		contentPane.add(label_create_res_4);
+		Label label_createres_4 = new Label("Contact Number:");
+		label_createres_4.setForeground(Color.WHITE);
+		label_createres_4.setFont(new Font("Calibri", Font.PLAIN, 12));
+		label_createres_4.setBounds(628, 393, 132, 21);
+		contentPane.add(label_createres_4);
 		
-		Label label_create_res_5 = new Label("Comments:");
-		label_create_res_5.setForeground(Color.WHITE);
-		label_create_res_5.setFont(new Font("Calibri", Font.PLAIN, 12));
-		label_create_res_5.setBounds(726, 481, 109, 21);
-		contentPane.add(label_create_res_5);
+		Label label_createres_5 = new Label("Comments:");
+		label_createres_5.setForeground(Color.WHITE);
+		label_createres_5.setFont(new Font("Calibri", Font.PLAIN, 12));
+		label_createres_5.setBounds(628, 470, 109, 21);
+		contentPane.add(label_createres_5);
 		
 		Button back_button = new Button("Back");
 		back_button.setFont(new Font("Calibri", Font.BOLD, 13));
-		back_button.setForeground(new Color(255, 255, 255));
-		back_button.setBackground(new Color(51, 51, 51));
-		back_button.setBounds(0, 628, 98, 41);
+		back_button.setForeground(Color.BLACK);
+		back_button.setBackground(Color.LIGHT_GRAY);
+		back_button.setBounds(23, 621, 98, 48);
 		contentPane.add(back_button);
 		
-		Label label = new Label("DATE :");
+		Label label = new Label("Date of arrival:");
 		label.setForeground(new Color(255, 255, 255));
-		label.setFont(new Font("Calibri", Font.PLAIN, 15));
-		label.setBounds(43, 127, 87, 21);
+		label.setFont(new Font("Calibri", Font.PLAIN, 14));
+		label.setBounds(98, 162, 159, 21);
 		contentPane.add(label);
 		
 		JCalendar calendar = new JCalendar();
-		calendar.setBounds(124, 127, 427, 367);
+		calendar.setBounds(98, 189, 407, 350);
 		contentPane.add(calendar);
+		
+		
+		JLabel logolbl1 = new JLabel("");
+		Image img = new ImageIcon(this.getClass().getResource("/Logo.png")).getImage();
+		logolbl1.setIcon(new ImageIcon(img));
+		logolbl1.setBounds(23, 32, 191, 64);
+		contentPane.add(logolbl1);
+		
+		JLabel phonelbl = new JLabel("");
+		Image img1 = new ImageIcon(this.getClass().getResource("/phone.png")).getImage();
+		phonelbl.setIcon(new ImageIcon(img1));
+		phonelbl.setBounds(574, 411, 48, 48);
+		contentPane.add(phonelbl);
+		
+		JLabel timelbl = new JLabel("");
+		Image img2 = new ImageIcon(this.getClass().getResource("/time.png")).getImage();
+		timelbl.setIcon(new ImageIcon(img2));
+		timelbl.setBounds(574, 336, 48, 48);
+		contentPane.add(timelbl);
+		
+		JLabel peoplelbl = new JLabel("");
+		Image img3 = new ImageIcon(this.getClass().getResource("/people.png")).getImage();
+		peoplelbl.setIcon(new ImageIcon(img3));
+		peoplelbl.setBounds(574, 260, 48, 48);
+		contentPane.add(peoplelbl);
+		
+		JLabel personlbl = new JLabel("");
+		Image img4 = new ImageIcon(this.getClass().getResource("/person.png")).getImage();
+		personlbl.setIcon(new ImageIcon(img4));
+		personlbl.setBounds(574, 196, 48, 48);
+		contentPane.add(personlbl);
+		
+		JLabel tablelbl = new JLabel("");
+		Image img5 = new ImageIcon(this.getClass().getResource("/table.png")).getImage();
+		tablelbl.setIcon(new ImageIcon(img5));
+		tablelbl.setBounds(574, 114, 48, 48);
+		contentPane.add(tablelbl);
+		
+		JLabel commentslbl = new JLabel("");
+		Image img6 = new ImageIcon(this.getClass().getResource("/comments.png")).getImage();
+		commentslbl.setIcon(new ImageIcon(img6));
+		commentslbl.setBounds(574, 497, 48, 48);
+		contentPane.add(commentslbl);
 	}
 }
+
