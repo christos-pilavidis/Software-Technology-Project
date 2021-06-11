b1ded0caaf8e5d3a381e38c60804a0aecb7f06c6
christos-pilavidis <73226277+christos-pilavidis@users.noreply.github.com>
2021-06-11 03:33:22 +0300

    Update editprofile.java

--- editprofile.java
+++ editprofile.java
@@ -9,9 +9,12 @@ import java.awt.Label;
 import javax.swing.JLabel;
 import java.awt.Button;
 import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.JTextPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class editprofile extends JFrame {
@@ -41,7 +44,7 @@ public class editprofile extends JFrame {
 	 */
 	public editprofile() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1250, 730);
+		setBounds(100, 100, 1000, 730);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -57,49 +60,55 @@ public class editprofile extends JFrame {
 		Label edit_profile = new Label("Edit profile ");
 		edit_profile.setForeground(Color.WHITE);
 		edit_profile.setFont(new Font("Calibri", Font.BOLD, 15));
-		edit_profile.setBounds(1065, 0, 171, 42);
+		edit_profile.setBounds(815, 0, 171, 42);
 		contentPane.add(edit_profile);
 		
 		JTextArea txtrOwnersName = new JTextArea();
 		txtrOwnersName.setText("Owner's name :");
-		txtrOwnersName.setFont(new Font("Calibri", Font.BOLD, 15));
-		txtrOwnersName.setBounds(404, 204, 364, 24);
+		txtrOwnersName.setFont(new Font("Calibri", Font.BOLD, 18));
+		txtrOwnersName.setBounds(430, 230, 364, 32);
 		contentPane.add(txtrOwnersName);
 		
 		JTextArea txtrBusinessName = new JTextArea();
-		txtrBusinessName.setFont(new Font("Calibri", Font.BOLD, 15));
+		txtrBusinessName.setFont(new Font("Calibri", Font.BOLD, 18));
 		txtrBusinessName.setText("Business name :");
-		txtrBusinessName.setBounds(404, 295, 364, 24);
+		txtrBusinessName.setBounds(430, 305, 364, 32);
 		contentPane.add(txtrBusinessName);
 		
 		txtTelephoneNumber = new JTextField();
 		txtTelephoneNumber.setText("Telephone number:");
-		txtTelephoneNumber.setFont(new Font("Calibri", Font.BOLD, 15));
-		txtTelephoneNumber.setBounds(404, 374, 364, 24);
+		txtTelephoneNumber.setFont(new Font("Calibri", Font.BOLD, 18));
+		txtTelephoneNumber.setBounds(430, 380, 364, 32);
 		contentPane.add(txtTelephoneNumber);
 		txtTelephoneNumber.setColumns(10);
 		
 		txtEmail = new JTextField();
-		txtEmail.setFont(new Font("Calibri", Font.BOLD, 15));
+		txtEmail.setFont(new Font("Calibri", Font.BOLD, 18));
 		txtEmail.setText("Email :");
-		txtEmail.setBounds(404, 455, 364, 24);
+		txtEmail.setBounds(430, 455, 364, 32);
 		contentPane.add(txtEmail);
 		txtEmail.setColumns(10);
 		
-		JButton btnNewButton = new JButton("SAVE");
-		btnNewButton.setFont(new Font("Calibri", Font.BOLD, 22));
-		btnNewButton.setBounds(1115, 650, 121, 33);
+		Button btnNewButton = new Button("Save \r\nchanges");
+		btnNewButton.setFont(new Font("Calibri", Font.BOLD, 15));
+		btnNewButton.setBounds(653, 635, 141, 42);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("BACK");
-		btnNewButton_1.setFont(new Font("Calibri", Font.BOLD, 22));
-		btnNewButton_1.setBounds(10, 650, 121, 33);
+		Button btnNewButton_1 = new Button("Back");
+		btnNewButton_1.setFont(new Font("Calibri", Font.BOLD, 15));
+		btnNewButton_1.setBounds(10, 635, 98, 48);
 		contentPane.add(btnNewButton_1);
 		
-		JLabel lblNewLabel = new JLabel("Business info ");
-		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 15));
+		JLabel lblNewLabel = new JLabel("Business info :");
+		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 23));
 		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(404, 135, 140, 24);
+		lblNewLabel.setBounds(430, 178, 178, 24);
 		contentPane.add(lblNewLabel);
+		
+		JLabel logolbl1 = new JLabel("");
+		Image img = new ImageIcon(this.getClass().getResource("/Logo.png")).getImage();
+		logolbl1.setIcon(new ImageIcon(img));
+		logolbl1.setBounds(20, 42, 191, 64);
+		contentPane.add(logolbl1);
 	}
 }
